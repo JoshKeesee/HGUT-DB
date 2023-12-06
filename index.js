d87ebfd85f6ce1f7dd0e3d4ce0691c67b255c59e
JoshKeesee <105324950+JoshKeesee@users.noreply.github.com>
2023-12-06 18:38:19 +0000

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -20,7 +21,7 @@ const pushKeys = {
 webpush.setVapidDetails(
   "mailto:joshuakeesee1@gmail.com",
   pushKeys.public,
-  pushKeys.private,
+  pushKeys.private
 );
 const profiles = require("./profiles.json");
 Object.keys(profiles).forEach((p) => {
@@ -75,23 +76,23 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use("/profiles", express.static(__dirname + "/profiles"));
 app.use("/images", express.static(__dirname + "/images"));
+app.use(cors());
 app.post("/subscribe", (req, res) => {
-  if (!req.user) return res.status(201).json({});
+	const user = profiles[req.body.user];
+  if (!user) return res.status(201).json({});
   const subscriptions = get("subscriptions") || {};
-  const s = req.body;
-  if (!subscriptions[req.user.id]) subscriptions[req.user.id] = [];
-  if (!subscriptions[req.user.id].length)
-    subscriptions[req.user.id] = [subscriptions[req.user.id]];
-  if (subscriptions[req.user.id].includes(s)) return;
-  subscriptions[req.user.id].push(s);
+  const s = req.body.subscription;
+  if (!subscriptions[user.id]) subscriptions[user.id] = {};
+  if (req.body.mobile) subscriptions[user.id].mobile = s;
+	else subscriptions[user.id].web = s;
   set({ subscriptions });
   res.status(201).json({});
 });
 app.post("/message", (req, res) => {
-  if (!req.user) return res.status(201).json({});
+  if (!req.body.user) return res.status(201).json({});
   const r = req.body;
-  req.user.room = r.room;
-  sendMessage(r.message, req.user, "chat", true);
+  r.user.room = r.room;
+  sendMessage(r.message, r.user, "chat", true);
 });
 app.get("/p", (req, res) => {
   res.json(profiles);
@@ -225,7 +226,7 @@ io.of("chat").on("connection", (socket) => {
     else if (!t && typing[socket.user.room].includes(socket.user.id))
       typing[socket.user.room].splice(
         typing[socket.user.room].indexOf(socket.user.id),
-        1,
+        1
       );
     io.of(curr).to(socket.user.room).emit("typing", typing[socket.user.room]);
   });
@@ -237,7 +238,7 @@ io.of("chat").on("connection", (socket) => {
     if (typing[socket.user.room].includes(socket.user.id))
       typing[socket.user.room].splice(
         typing[socket.user.room].indexOf(socket.user.id),
-        1,
+        1
       );
     io.of(curr).to(socket.user.room).emit("typing", typing[socket.user.room]);
     io.of(curr).emit("online", o);
@@ -255,8 +256,8 @@ io.of("chat").on("connection", (socket) => {
       "load messages",
       m.slice(
         Math.max(0, m.length - lm - maxMessages),
-        Math.max(0, m.length - lm),
-      ),
+        Math.max(0, m.length - lm)
+      )
     );
   });
 
@@ -283,7 +284,7 @@ io.of("chat").on("connection", (socket) => {
     if (typing[socket.user.room].includes(socket.user.id))
       typing[socket.user.room].splice(
         typing[socket.user.room].indexOf(socket.user.id),
-        1,
+        1
       );
     socket.leave(socket.user.room);
     io.of(curr).to(socket.user.room).emit("typing", typing[socket.user.room]);
@@ -296,7 +297,7 @@ io.of("chat").on("connection", (socket) => {
     if (socket.user.unread.includes(socket.user.room))
       socket.user.unread.splice(
         socket.user.unread.indexOf(socket.user.room),
-        1,
+        1
       );
     const users = get("users") || {};
     users[socket.user.id] = socket.user;
@@ -328,8 +329,9 @@ const sendMessage = (message, us, curr, p = false) => {
     isImage = true;
   }
   if (message.length > 250) return;
+	const rooms = get("rooms");
+	const lastMessage = rooms[us.room].messages[rooms[us.room].messages.length - 1];
   if (curr == "chat") {
-    const rooms = get("rooms");
     rooms[us.room].messages.push({ message, name: us.name, date: new Date() });
     set({ rooms });
     const users = get("users") || {};
@@ -357,9 +359,9 @@ const sendMessage = (message, us, curr, p = false) => {
             },
           ],
         });
-        if (!subscriptions[u.id].length)
+        if (!subscriptions[u.id]?.length)
           subscriptions[u.id] = [subscriptions[u.id]];
-        subscriptions[u.id].forEach((e) => {
+        Object.values(subscriptions[u.id]).forEach((e) => {
           webpush.sendNotification(e, payload).catch((e) => {});
         });
       }
@@ -374,8 +376,9 @@ const sendMessage = (message, us, curr, p = false) => {
     if (typing[us.room].includes(us.id) && !p)
       typing[us.room].splice(typing[us.room].indexOf(us.id), 1);
     if (!p) io.of(curr).to(us.room).emit("typing", typing[us.room]);
-    io.of(curr).to(us.room).emit("chat message", [message, us, new Date()]);
-  } else io.of(curr).emit("chat message", [message, us, new Date()]);
+		const m = [message, us, new Date(), lastMessage, rooms[us.room].allowed];
+		io.of(curr).emit("chat message", m);
+  } else io.of(curr).emit("chat message", [message, us, new Date(), lastMessage]);
 };
 
 server.listen(3000, () => {
