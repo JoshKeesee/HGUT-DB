93bba1aeaee446e1931fb12e1fd34cd908998631
Joshua Keesee <joshuakeesee1@gmail.com>
2024-02-21 18:48:43 -0500

    Update user authentication and profile image

--- io-auth.js
+++ io-auth.js
@@ -2,7 +2,7 @@ const cookie = require("cookie");
 const checkUser = require("./check-user");
 
 module.exports = (socket, next) => {
-  const h = socket.handshake.query;
+  const h = socket.handshake.headers;
   if (!h.user) return next();
   const username = cookie.parse(h.user)["user"];
   socket.user = checkUser(username);
